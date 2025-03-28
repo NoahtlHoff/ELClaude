15bc95bb07d9051044d301657e7a1b3eb43c4349
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-03-28 10:39:09 +0100

    Cleaned up file

--- equilog-backend/Program.cs
+++ equilog-backend/Program.cs
@@ -4,7 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // This is a comment!
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddAuthorization();

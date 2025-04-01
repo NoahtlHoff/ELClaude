95a7e8b492a15f7cff0d76a95d2160c32565c679
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-01 13:25:12 +0200

    Created service method for fetching all events from the database

--- equilog-backend/Services/EventService.cs
+++ equilog-backend/Services/EventService.cs
@@ -1,8 +1,24 @@
-﻿using equilog_backend.Data;
+﻿using equilog_backend.Common;
+using equilog_backend.Data;
+using equilog_backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace equilog_backend.Services;
 
 public class EventService(EquilogDbContext context)
 {
-    
+    public async Task<Response<List<Event>?>> GetAllEvents()
+    {
+        try
+        {
+            var events = await context.Events
+                .ToListAsync();
+
+            return Response<List<Event>>.Success(events);
+        }
+        catch (Exception ex)
+        {
+            return Response<List<Event>>.Failure(ex.Message);
+        }
+    }
 }

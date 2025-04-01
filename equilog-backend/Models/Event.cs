85ac1f731b97b66faa4b97c9b0ff7945cb1a2e26
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-01 11:46:24 +0200

    Created Event class and added properties

--- equilog-backend/Models/Event.cs
+++ equilog-backend/Models/Event.cs
@@ -1,6 +1,17 @@
-﻿namespace equilog_backend.Models;
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.Models;
 
 public class Event
 {
-    
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    public required string Title { get; set; }
+
+    public DateTime StartDateTime { get; set; }
+
+    public DateTime EndDateTime { get; set; }
 }

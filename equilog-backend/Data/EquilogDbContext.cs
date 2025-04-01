87e2315db8fc85218b3d9d5d4c9beffd2925b06f
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-01 12:20:47 +0200

    Added Event DbSet to DbContext class

--- equilog-backend/Data/EquilogDbContext.cs
+++ equilog-backend/Data/EquilogDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using equilog_backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace equilog_backend.Data;
 
 public class EquilogDbContext(DbContextOptions<EquilogDbContext> options)
     :DbContext(options)
 {
-    
+    public DbSet<Event> Events { get; set; }
 }

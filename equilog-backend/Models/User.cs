527e948d23d3f25fa0314e573bce35c3843fd508
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-02 11:34:02 +0200

    Created User model

--- equilog-backend/Models/User.cs
+++ equilog-backend/Models/User.cs
@@ -1,6 +1,23 @@
-﻿namespace equilog_backend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.Models;
 
 public class User
 {
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    public required string FirstName { get; set; }
+
+    [StringLength(50)]
+    public required string LastName { get; set; }
     
+    [StringLength(254)]
+    public required string Email { get; set; }
+
+    [StringLength(20)]
+    public string? PhoneNumber { get; set; }
+    
+    public virtual List<UserHorse>? UserHorses { get; set; }
 }

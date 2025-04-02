26b1f9b3d49e7a38b2d06c6daa43ea345d836622
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-02 14:40:49 +0200

    Added DbSets

--- equilog-backend/Data/EquilogDbContext.cs
+++ equilog-backend/Data/EquilogDbContext.cs
@@ -6,6 +6,13 @@ namespace equilog_backend.Data;
 public class EquilogDbContext(DbContextOptions<EquilogDbContext> options)
     :DbContext(options)
 {
-    public DbSet<Event> Events { get; set; }
+    public DbSet<User> Users { get; set; }
     public DbSet<Horse> Horses { get; set; }
+    public DbSet<Stable> Stables { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<UserStable> UserStables { get; set; }
+    public DbSet<UserHorse> UserHorses { get; set; }
+    public DbSet<StableHorse> StableHorses { get; set; }
+    public DbSet<UserEvent> UserEvents { get; set; }
+    
 }

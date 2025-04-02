3dc988be95a13d4da2c6ee154e7f4a4aab8b1fd8
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-02 11:34:39 +0200

    Adde properties to UserHorse relationship model

--- equilog-backend/UserHorse.cs
+++ equilog-backend/UserHorse.cs
@@ -1,6 +1,19 @@
-﻿namespace equilog_backend;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using equilog_backend.Models;
+
+namespace equilog_backend;
 
 public class UserHorse
 {
+    [Key]
+    public int Id { get; set; }
+
+    [ForeignKey("User")]
+    public required int UserIdFk { get; set; }
+    public virtual required User User { get; set; }
     
+    [ForeignKey("Horse")]
+    public required int HorseIdFk { get; set; }
+    public virtual required Horse Horse { get; set; }
 }

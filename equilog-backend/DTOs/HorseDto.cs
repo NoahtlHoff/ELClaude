994627d57da13f00014acbcf5d09e8d1dc6557bc
NoahtlHoff <noah.hoff@outlook.com>
2025-04-03 16:46:06 +0200

    Created HorseDto without validation strictly for mapping and output.

---
+++ equilog-backend/DTOs/HorseDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs
+{
+    public class HorseDTO
+    {
+        // for mapping/output ONLY
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? Age { get; set; }
+        public string? Color { get; set; }
+        public string? Breed { get; set; }
+    }
+}

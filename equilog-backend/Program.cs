58b598b71ac67010903c87814eea414d2506d5f7
NoahtlHoff <noah.hoff@outlook.com>
2025-04-03 17:45:08 +0200

    Added builder to configure AutoMapper

--- equilog-backend/Program.cs
+++ equilog-backend/Program.cs
@@ -15,6 +15,7 @@ public class Program
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         builder.Services.AddDbContext<EquilogDbContext>(options =>
         {

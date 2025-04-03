fbda7c449b03023bee57c22c1fe06dba0dec25d9
NoahtlHoff <noah.hoff@outlook.com>
2025-04-03 18:36:25 +0200

    added temporary test to see if mapping configured correctly

---
+++ equilog-backend/Mapping/AutoMapperTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+namespace equilog_backend.Mapping
+{
+    public class AutoMapperTests
+    {
+        public void AutoMapper_Configuration_IsValid()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<HorseProfile>();
+            });
+
+            try
+            {
+                config.AssertConfigurationIsValid();
+                Console.WriteLine("AutoMapper configuration is valid.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

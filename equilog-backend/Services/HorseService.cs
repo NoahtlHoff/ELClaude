a9efa12b431a19e730397faa2e144876621e92cc
NoahtlHoff <noah.hoff@outlook.com>
2025-04-03 18:16:28 +0200

    Fixed HorseService to work with IMapper

--- equilog-backend/Services/HorseService.cs
+++ equilog-backend/Services/HorseService.cs
@@ -2,22 +2,24 @@
 using equilog_backend.Models;
 using Microsoft.EntityFrameworkCore;
 using equilog_backend.Common;
+using AutoMapper;
+using equilog_backend.DTOs;
 namespace equilog_backend.Services
 {
-    public class HorseService(EquilogDbContext context)
+    public class HorseService(EquilogDbContext context, IMapper mapper)
     {
-        public async Task<Response<List<Horse>?>> GetAllHorses()
+        public async Task<Response<List<HorseDto>?>> GetAllHorses()
         {
             try
             {
-                var horses = await context.Horses
-                    .ToListAsync();
+                var horses = await context.Horses.ToListAsync();
+                var horseDtos = mapper.Map<List<HorseDto>>(horses);
 
-                return Response<List<Horse>>.Success(horses);
+                return Response<List<HorseDto>>.Success(horseDtos);
             }
             catch (Exception ex)
             {
-                return Response<List<Horse>>.Failure(ex.Message);
+                return Response<List<HorseDto>>.Failure(ex.Message);
             }
         }
     }

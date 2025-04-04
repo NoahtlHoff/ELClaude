5704bc3f5d4e363da2c9a7daeb2face3fdfbfa16
NoahtlHoff <noah.hoff@outlook.com>
2025-04-04 12:32:10 +0200

    Created UserService and added method to get all users

---
+++ equilog-backend/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using equilog_backend.Common;
+using equilog_backend.Data;
+using equilog_backend.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace equilog_backend.Services
+{
+    public class UserService(EquilogDbContext context, IMapper mapper)
+    {
+        public async Task<Response<List<UserDto>?>> GetAllUsers()
+        {
+            try
+            {
+                var userDtos = mapper.Map<List<UserDto>>(await context.Users.ToListAsync());
+
+                return Response<List<UserDto>>.Success(userDtos);
+            }
+            catch (Exception ex)
+            {
+                return Response<List<UserDto>>.Failure(ex.Message);
+            }
+        }
+    }
+}

084924b68b3f72fadd1265f590fd83275cd5ef86
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-05 14:56:17 +0200

    Configured CORS and changed config and init architecture

---
+++ equilog-backend/Startup/AppInitialization.cs
@@ -0,0 +1,36 @@
+﻿using equilog_backend.Endpoints;
+
+namespace equilog_backend.Startup;
+
+public class AppInitialization
+{
+    public static void InitializeDevEnvironment(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+    }
+
+    public static void InitializeHttpRedirection(WebApplication app)
+    {
+        app.UseHttpsRedirection();
+    }
+    
+    public static void InitializeCors(WebApplication app)
+    {
+        app.UseCors("Default");
+    }
+
+    public static void InitializeAuthorization(WebApplication app)
+    {
+        app.UseAuthorization();
+    }
+
+    public static void InitializeEndpoints(WebApplication app)
+    {
+        EventEndpoints.RegisterEndpoints(app);
+        HorseEndpoints.RegisterEndpoints(app);
+    }
+}

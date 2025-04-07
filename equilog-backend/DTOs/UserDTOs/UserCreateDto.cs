3d97d2f170b742c902864959fd4b60297b76ad47
NoahtlHoff <noah.hoff@outlook.com>
2025-04-07 16:33:27 +0200

    Added a Dto for creating new users

---
+++ equilog-backend/DTOs/UserDTOs/UserCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.UserDTOs
+{
+    public class UserCreateDto
+    {
+        [Required]
+        public required int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string LastName { get; set; }
+
+        [Required]
+        [StringLength(254)]
+        public required string Email { get; set; }
+
+        [StringLength(20)]
+        public string? PhoneNumber { get; set; }
+    }
+}

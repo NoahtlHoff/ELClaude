faba6f1dfd8e4f2e46ae7806a7df4ae19e42a9b5
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-07 13:02:37 +0200

    Created endpoint and method for updating event. Also changed some route names

--- equilog-backend/Endpoints/CalendarEventEndpoints.cs
+++ equilog-backend/Endpoints/CalendarEventEndpoints.cs
@@ -2,6 +2,7 @@
 using equilog_backend.Common;
 using equilog_backend.Common.Enums;
 using equilog_backend.DTOs;
+using equilog_backend.DTOs.CalendarEventDTOs;
 using equilog_backend.Services;
 
 namespace equilog_backend.Endpoints;
@@ -10,11 +11,17 @@ public class CalendarEventEndpoints
 {
     public static void RegisterEndpoints(WebApplication app)
     {
-        app.MapGet("/events", GetEvents)
+        // Get all calendar events.
+        app.MapGet("/api/calendar-event", GetEvents)
             .WithName("GetEvents");
 
-        app.MapGet("api/calendar-event", GetEvent)
+        // Get calendar event by id.
+        app.MapGet("api/calendar-event/{id:int}", GetEvent)
             .WithName("GetEvent");
+
+        // Update calendar event.
+        app.MapGet("/api/calendar-event/update", UpdateEvent)
+            .WithName("UpdateEvent");
     }
 
     private static async Task<IResult> GetEvents(CalendarEventService calendarEventService)
@@ -31,6 +38,19 @@ public class CalendarEventEndpoints
     private static async Task<IResult> GetEvent(CalendarEventService calendarEventService, int id)
     {
         var apiResponse = await calendarEventService.GetEvent(id);
+
+        return apiResponse.StatusCode switch
+        {
+            HttpStatusCode.OK => Results.Ok(apiResponse),
+            HttpStatusCode.NotFound => Results.NotFound(apiResponse),
+            _ => Results.Problem(apiResponse.Message, statusCode: 500)
+        };
+    }
+
+    private static async Task<IResult> UpdateEvent(CalendarEventService calendarEventService,
+        CalendarEventUpdateDto updatedEvent)
+    {
+        var apiResponse = await calendarEventService.UpdateCalendarEvent(updatedEvent);
 
         return apiResponse.StatusCode switch
         {

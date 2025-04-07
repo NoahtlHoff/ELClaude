84747b47baefa0d4cee61b5efe496efdec5a89ce
NoahtlHoff <noah.hoff@outlook.com>
2025-04-07 16:18:25 +0200

    Added more endpoints for user

--- equilog-backend/Endpoints/UserEndpoints.cs
+++ equilog-backend/Endpoints/UserEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using equilog_backend.DTOs.UserDTOs;
 using equilog_backend.Services;
 
 namespace equilog_backend.Endpoints
@@ -7,16 +8,81 @@ namespace equilog_backend.Endpoints
     {
         public static void RegisterEndpoints(WebApplication app)
         {
-            app.MapGet("/users", async (UserService userService) =>
-            {
-                var response = await userService.GetAllUsers();
+            
+            // Get all users.
+            app.MapGet("/api/user", GetUsers)
+                .WithName("Getusers");
 
-                return response.StatusCode switch
-                {
-                    HttpStatusCode.OK => Results.Ok(response.Value),
-                    _ => Results.Problem(response.Message, statusCode: 500)
-                };
-            });
+            // Get user.
+            app.MapGet("/api/user/{id:int}", GetUser)
+                .WithName("GetUser");
+
+            // Create user.
+            app.MapPost("/api/user", CreateUser)
+                .WithName("CreateUser");
+
+            // Update user properties.
+            app.MapPut("/api/user", UpdateUser)
+                .WithName("UpdateUser");
+
+            // Delete user.
+            app.MapDelete("/api/user/{id:int}", DeleteUser)
+                .WithName("DeleteUser");
+        }
+
+        private static async Task<IResult> GetUsers(UserService userService)
+        {
+            var apiResponse = await userService.GetAllUsers();
+
+            return apiResponse.StatusCode switch
+            {
+                HttpStatusCode.OK => Results.Ok(apiResponse.Value),
+                _ => Results.Problem(apiResponse.Message, statusCode: 500)
+            };
+        }
+        private static async Task<IResult> GetUser(UserService userService, int id)
+        {
+            var apiResponse = await userService.GetUser(id);
+
+            return apiResponse.StatusCode switch
+            {
+                HttpStatusCode.OK => Results.Ok(apiResponse.Value),
+                _ => Results.Problem(apiResponse.Message, statusCode: 500)
+            };
+        }
+        private static async Task<IResult> CreateUser(UserService userService, UserCreateDto newUser)
+        {
+            var apiResponse = await userService.CreateUser(newUser);
+
+            return apiResponse.StatusCode switch
+            {
+                HttpStatusCode.Created => Results.Json(apiResponse, statusCode: 201),
+                _ => Results.Problem(apiResponse.Message, statusCode: 500)
+            };
+        }
+
+        private static async Task<IResult> UpdateUser(UserService userService, UserUpdateDto updatedUser)
+        {
+            var apiResponse = await userService.UpdateUser(updatedUser);
+
+            return apiResponse.StatusCode switch
+            {
+                HttpStatusCode.OK => Results.Ok(apiResponse),
+                HttpStatusCode.NotFound => Results.NotFound(apiResponse),
+                _ => Results.Problem(apiResponse.Message, statusCode: 500)
+            };
+        }
+
+        private static async Task<IResult> DeleteUser(UserService userService, int id)
+        {
+            var apiResponse = await userService.DeleteUser(id);
+
+            return apiResponse.StatusCode switch
+            {
+                HttpStatusCode.NoContent => Results.Json(apiResponse, statusCode: 204),
+                HttpStatusCode.NotFound => Results.NotFound(apiResponse),
+                _ => Results.Problem(apiResponse.Message, statusCode: 500)
+            };
         }
     }
 }

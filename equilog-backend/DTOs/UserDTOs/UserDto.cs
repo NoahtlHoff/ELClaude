1b04178df4f26946a20e79f11abcdc308266b880
NoahtlHoff <noah.hoff@outlook.com>
2025-04-07 16:34:37 +0200

    Moved UserDto to a new UserDto folder

---
+++ equilog-backend/DTOs/UserDTOs/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.UserDTOs
+{
+    public class UserDto
+    {
+        [Required]
+        public required int Id { get; set; }
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [Required]
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}

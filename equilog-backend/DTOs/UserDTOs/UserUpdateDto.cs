c63f634b361b87e58e6b4c74aa65dad31a17ba69
NoahtlHoff <noah.hoff@outlook.com>
2025-04-07 16:35:23 +0200

    Added Dto for updating existing users

---
+++ equilog-backend/DTOs/UserDTOs/UserUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.UserDTOs
+{
+    public class UserUpdateDto
+    {
+        [Required]
+        public required int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+    }
+}

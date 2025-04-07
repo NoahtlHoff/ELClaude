d3e0cff44fa95c1a92976773af61eadc7e9ed671
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-07 12:25:52 +0200

    Added id property

--- equilog-backend/DTOs/CalendarEventDTOs/CalendarEventUpdateDto.cs
+++ equilog-backend/DTOs/CalendarEventDTOs/CalendarEventUpdateDto.cs
@@ -5,5 +5,8 @@ namespace equilog_backend.DTOs.CalendarEventDTOs;
 public class CalendarEventUpdateDto
 {
     [Required]
+    public required int Id { get; set; }
+    
+    [Required]
     public required string Title { get; set; }
 }

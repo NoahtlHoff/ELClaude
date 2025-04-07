f2ea4eacdce629b56e1cba57d41bffb0c471855f
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-07 17:23:24 +0200

    Removed required keyword from properties

--- equilog-backend/Models/UserHorse.cs
+++ equilog-backend/Models/UserHorse.cs
@@ -10,12 +10,12 @@ public class UserHorse
     public int Id { get; set; }
 
     [ForeignKey("User")]
-    public int UserIdFk { get; set; }
-    public virtual required User User { get; set; }
+    public required int UserIdFk { get; set; }
+    public virtual User? User { get; set; }
     
     [ForeignKey("Horse")]
-    public int HorseIdFk { get; set; }
-    public virtual required Horse Horse { get; set; }
+    public required int HorseIdFk { get; set; }
+    public virtual Horse? Horse { get; set; }
 
     public required string UserRole { get; set; } // Change to enum.
 }

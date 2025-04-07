922ddeb1c5aae0b8115c0a60408ad942ecf58495
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-07 12:14:54 +0200

    Created CalendarEventUpdateDto class file

--- equilog-backend/DTOs/CalendarEventDTOs/CalendarEventUpdateDto.cs
+++ equilog-backend/DTOs/CalendarEventDTOs/CalendarEventUpdateDto.cs
@@ -1,6 +1,9 @@
-﻿namespace equilog_backend.DTOs.CalendarEventDTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.CalendarEventDTOs;
 
 public class CalendarEventUpdateDto
 {
-    
+    [Required]
+    public required string Title { get; set; }
 }

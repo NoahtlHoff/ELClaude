fb072232f361917bfdb7742440b66966afbb120a
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-08 17:16:51 +0200

    Created IStableService interface

---
+++ equilog-backend/Interfaces/IStableService.cs
@@ -0,0 +1,17 @@
+﻿using equilog_backend.Common;
+using equilog_backend.DTOs.StableDTOs;
+
+namespace equilog_backend.Interfaces;
+
+public interface IStableService
+{
+    public Task<ApiResponse<List<StableDto>?>> GetStables();
+
+    public Task<ApiResponse<StableDto?>> GetStable();
+
+    public Task<ApiResponse<StableDto?>> CreateStable();
+
+    public Task<ApiResponse<StableDto?>> UpdateStable();
+
+    public Task<ApiResponse<StableDto?>> DeleteStable();
+}

7168e1464a651310a7ace3048692111f44df0b72
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-08 17:22:55 +0200

    Added service to get all stables

--- equilog-backend/Services/StableService.cs
+++ equilog-backend/Services/StableService.cs
@@ -1,12 +1,29 @@
-﻿using AutoMapper;
+﻿using System.Net;
+using AutoMapper;
 using equilog_backend.Common;
 using equilog_backend.Data;
 using equilog_backend.DTOs.StableDTOs;
 using equilog_backend.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace equilog_backend.Services;
 
 public class StableService(EquilogDbContext context, IMapper mapper)
 {
-   
+   public async Task<ApiResponse<List<StableDto>?>> GetStables()
+   {
+      try
+      {
+         var stableDtos = mapper.Map<List<StableDto>>(await context.Stables.ToListAsync());
+
+         return ApiResponse<List<StableDto>>.Success(HttpStatusCode.OK,
+            stableDtos,
+            null);
+      }
+      catch (Exception ex)
+      {
+         return ApiResponse<List<StableDto>>.Failure(HttpStatusCode.InternalServerError,
+            ex.Message);
+      }
+   }
 }

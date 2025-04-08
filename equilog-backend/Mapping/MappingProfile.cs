3b7ca61579a81fc784b3814fddf561ede5a63f99
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-08 20:09:28 +0200

    Added stable post mappings

--- equilog-backend/Mapping/MappingProfile.cs
+++ equilog-backend/Mapping/MappingProfile.cs
@@ -3,6 +3,7 @@ using equilog_backend.Models;
 using equilog_backend.DTOs.CalendarEventDTOs;
 using equilog_backend.DTOs.HorseDTOs;
 using equilog_backend.DTOs.StableDTOs;
+using equilog_backend.DTOs.StablePostDTOs;
 using equilog_backend.DTOs.UserDTOs;
 
 namespace equilog_backend.Mapping
@@ -22,6 +23,10 @@ namespace equilog_backend.Mapping
             CreateMap<Stable, StableDto>().ReverseMap();
             CreateMap<StableCreateDto, Stable>();
             CreateMap<StableUpdateDto, Stable>();
+
+            CreateMap<StablePost, StablePostDto>().ReverseMap();
+            CreateMap<StablePostCreateDto, StablePost>();
+            CreateMap<StablePostUpdateDto, StablePost>();
 
             CreateMap<CalendarEvent, CalendarEventDto>().ReverseMap();
             CreateMap<CalendarEventCreateDto, CalendarEvent>();

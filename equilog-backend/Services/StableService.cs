f1c8b76cbc541c458b1702bbe3f0e03a0e55febb
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-08 17:18:49 +0200

    Created StableService class

---
+++ equilog-backend/Services/StableService.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using equilog_backend.Common;
+using equilog_backend.Data;
+using equilog_backend.DTOs.StableDTOs;
+using equilog_backend.Interfaces;
+
+namespace equilog_backend.Services;
+
+public class StableService(EquilogDbContext context, IMapper mapper)
+{
+   
+}

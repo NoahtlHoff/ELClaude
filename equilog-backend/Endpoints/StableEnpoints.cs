7dac800b4b50db23de250cc4dce333344281c366
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-08 17:34:21 +0200

    Created response method for GetStables functionality

--- equilog-backend/Endpoints/StableEnpoints.cs
+++ equilog-backend/Endpoints/StableEnpoints.cs
@@ -1,9 +1,23 @@
-﻿namespace equilog_backend.Endpoints;
+﻿using System.Net;
+using equilog_backend.Interfaces;
+
+namespace equilog_backend.Endpoints;
 
 public class StableEnpoints
 {
     public static void RegisterEndpoints(WebApplication app)
     {
         
+    }
+
+    public async Task<IResult> GetStables(IStableService stableService)
+    {
+        var apiResponse = await stableService.GetStables();
+
+        return apiResponse.StatusCode switch
+        {
+            HttpStatusCode.OK => Results.Ok(apiResponse),
+            _ => Results.Problem(apiResponse.Message, statusCode: 500)
+        };
     }
 }

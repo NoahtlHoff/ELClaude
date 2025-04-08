d344658377039b2c31af32cc9cd05c3539a188cd
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-08 20:02:06 +0200

    Created DTO's for StablePost class

---
+++ equilog-backend/DTOs/StablePostDTOs/StablePostCreateDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.StablePostDTOs;
+
+public class StablePostCreateDto
+{
+    [Required]
+    public required int UserIdFk { get; set; }
+
+    [Required]
+    public required int StableIdFk { get; set; }
+
+    [Required]
+    [StringLength(510)]
+    public required string Title { get; set; }
+
+    [Required]
+    [StringLength(4094)]
+    public required string Content { get; set; }
+
+    [Required]
+    public required DateTime Date { get; set; }
+
+    [Required]
+    public required bool IsPinned { get; set; }
+}
---
+++ equilog-backend/DTOs/StablePostDTOs/StablePostUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.StablePostDTOs;
+
+public class StablePostUpdateDto
+{
+    [Required]
+    public required string Title { get; set; }
+
+    [Required]
+    public required string Content { get; set; }
+}

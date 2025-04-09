6c8407e06ee2d121fb49d47a842e1862ad106329
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-09 20:01:07 +0200

    Create AuthService file and added dependecies

---
+++ equilog-backend/Services/AuthService.cs
@@ -0,0 +1,10 @@
+﻿using equilog_backend.Data;
+using equilog_backend.Interfaces;
+using equilog_backend.Security;
+
+namespace equilog_backend.Services;
+
+public class AuthService(EquilogDbContext context, JwtSettings jwtSettings) : IAuthService
+{
+    
+}

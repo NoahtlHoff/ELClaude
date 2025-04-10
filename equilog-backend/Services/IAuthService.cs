6b6fbaaf6c74b6bfa90414685e23fc56e0a74d4d
NoahtlHoff <noah.hoff@outlook.com>
2025-04-10 19:48:03 +0200

    added interface for auth services

---
+++ equilog-backend/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using equilog_backend.Auth;
+using equilog_backend.Models;
+
+namespace equilog_backend.Interfaces;
+
+public interface IAuthService
+{
+    string GenerateToken(User user);
+    bool ValidateToken(string token);
+    User? GetUserFromToken(string token);
+}

82c8e548a794d378302a47444cb7cdef518d45e9
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-14 14:53:29 +0200

    Added migrations

---
+++ equilog-backend/Migrations/20250414125208_Init3.0_HorseAgeDateOnly_UserStableRole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace equilog_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init30_HorseAgeDateOnly_UserStableRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "UserStables",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Age",
+                table: "Horses",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "UserStables");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Age",
+                table: "Horses",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+        }
+    }
+}

d9a60761f5219b5e954670437d204b3650eb1167
NoahtlHoff <noah.hoff@outlook.com>
2025-04-14 17:16:09 +0200

    Created AuthUser for user login and auth ops

---
+++ equilog-backend/Models/Security/AuthUser.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace equilog_backend.Models.Security
+{
+    public class AuthUser
+    {
+        [Key]
+        public  int Id { get; set; }
+
+        public required string Email { get; set; }
+
+        public required byte[] PasswordHash { get; set; }
+
+        public required byte[] PasswordSalt { get; set; }
+
+        [ForeignKey("User")]
+        public required int UserIdFk { get; set; }
+        public virtual User? User { get; set; }
+    }
+}

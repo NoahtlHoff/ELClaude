c4a8de729130ea373b4cb04b61896f3034ef1cad
NoahtlHoff <noah.hoff@outlook.com>
2025-04-15 12:23:40 +0200

    Created Dtos for desplaying and updating wall-posts

---
+++ equilog-backend/DTOs/WallPostDTOs/WallPostDto.cs
@@ -0,0 +1,11 @@
+﻿namespace equilog_backend.DTOs.WallPostDTOs
+{
+    public class WallPostDto
+    {
+        public int StableIdFk { get; set; }
+        public string? Title { get; set; }
+        public string? Body { get; set; }
+        public DateTime? PostDate { get; set; }
+        public DateTime? LastEdited { get; set; }
+    }
+}
---
+++ equilog-backend/DTOs/WallPostDTOs/WallPostUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.WallPostDTOs
+{
+    public class WallPostUpdateDto
+    {
+        [Required]
+        public required int StableIdFk { get; set; }
+
+        [StringLength(40)]
+        public string? Title { get; set; }
+
+        [StringLength(280)]
+        public string? Body { get; set; }
+    }
+}

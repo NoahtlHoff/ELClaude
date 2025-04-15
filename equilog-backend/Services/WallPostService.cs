4ebd1603c4e4ad15f4cad02dd39ec91c069e9d63
NoahtlHoff <noah.hoff@outlook.com>
2025-04-15 12:57:29 +0200

    Added file for WallPost services

---
+++ equilog-backend/Services/WallPostService.cs
@@ -0,0 +1,19 @@
+﻿using equilog_backend.Common;
+using equilog_backend.DTOs.WallPostDTOs;
+using equilog_backend.Interfaces;
+
+namespace equilog_backend.Services
+{
+    public class WallPostService : IWallPostService
+    {
+        public Task<ApiResponse<WallPostDto?>> GetWallPost(int StableId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse<WallPostDto?>> UpdateWallPost(WallPostUpdateDto updatedWallPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

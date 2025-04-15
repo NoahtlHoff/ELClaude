e4b2bc48451ad2c76ef7769287078853dd42eca6
NoahtlHoff <noah.hoff@outlook.com>
2025-04-15 15:17:48 +0200

    Changed argument names

--- equilog-backend/Interfaces/IWallPostService.cs
+++ equilog-backend/Interfaces/IWallPostService.cs
@@ -7,8 +7,10 @@ namespace equilog_backend.Interfaces
     {
         public Task<ApiResponse<WallPostDto?>> GetWallPost(int StableId);
 
-        public Task<ApiResponse<WallPostDto?>> UpdateReplaceWallPost(WallPostUpdateDto updatedWallPost);
+        public Task<ApiResponse<WallPostDto?>> ReplaceWallPost(WallPostDto wallPostDto);
 
-        public Task<ApiResponse<WallPostDto?>> UpdateEditWallPost(WallPostUpdateDto editWallPost);
+        public Task<ApiResponse<WallPostDto?>> EditWallPost(WallPostDto wallPostDto);
+
+        public Task<ApiResponse<WallPostDto?>> ClearWallPost(WallPostDto wallPostDto);
     }
 }

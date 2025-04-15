b098e34d607c392866e3d6aa4462b3df12dc566c
NoahtlHoff <noah.hoff@outlook.com>
2025-04-15 11:43:19 +0200

    Created model posts on stable-homepages

---
+++ equilog-backend/Models/WallPost.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace equilog_backend.Models
+{
+    public class WallPost
+    {
+        [Key]
+        [ForeignKey("Stable")]
+        public int StableIdFk { get; set; }
+
+        [StringLength(50)]
+        public string? Title { get; set; }
+
+        [StringLength(280)]
+        public string? Body { get; set; }
+
+        public DateTime? PostDate { get; set; }
+        public DateTime? LastEdited { get; set; }
+    }
+}

423fdb70ad9801670990b55783941fd54bba43c9
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-15 11:58:15 +0200

    Created IAuthService interface and added GenerateToken, Register and Login methods

---
+++ equilog-backend/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using equilog_backend.Common;
+using equilog_backend.DTOs.AuthDTOs;
+using equilog_backend.Models;
+
+namespace equilog_backend.Interfaces;
+
+public interface IAuthService
+{
+    string GenerateToke(User user);
+    Task<ApiResponse<AuthResponseDto?>> Register(RegisterDto registerDto);
+    Task<ApiResponse<AuthResponseDto?>> Login(LoginDto loginDto);
+}

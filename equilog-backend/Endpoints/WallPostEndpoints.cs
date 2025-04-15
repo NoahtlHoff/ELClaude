16817c844db667280926990831d429e56ddc347c
NoahtlHoff <noah.hoff@outlook.com>
2025-04-15 13:53:23 +0200

    Added WallPostEndpoint with basic structure

---
+++ equilog-backend/Endpoints/WallPostEndpoints.cs
@@ -0,0 +1,33 @@
+﻿
+namespace equilog_backend.Endpoints
+{
+    public class WallPostEndpoints
+    {
+        public static void RegisterEndpoints(WebApplication app)
+        {
+            app.MapGet("/api/wallpost", GetWallPost)
+                .WithName("GetWallPost");
+
+            app.MapPut("/api/wallpost/replace", ReplaceWallPost)
+                .WithName("ReplaceWallPost");
+
+            app.MapPatch("/api/wallpost/edit", EditWallPost)
+                .WithName("EditWallPost");
+        }
+
+        private static async Task EditWallPost(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task ReplaceWallPost(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task GetWallPost(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

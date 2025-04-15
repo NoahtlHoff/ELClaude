e464cc02345c8bf36767613081d2d02357ee77b4
NoahtlHoff <noah.hoff@outlook.com>
2025-04-15 12:34:22 +0200

    Created interface for Wallpost-service

---
+++ equilog-backend/Interfaces/IWallPostService.cs
@@ -0,0 +1,12 @@
+﻿using equilog_backend.DTOs.WallPostDTOs;
+using equilog_backend.Common;
+
+namespace equilog_backend.Interfaces
+{
+    public interface IWallPostService
+    {
+        public Task<ApiResponse<WallPostDto?>> GetWallPost(int StableId);
+
+        public Task<ApiResponse<WallPostDto?>> UpdateWallPost(WallPostUpdateDto updatedWallPost);
+    }
+}

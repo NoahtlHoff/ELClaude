1353d47a03a8eaa43a6b9afb0e07ff672692a384
NoahtlHoff <noah.hoff@outlook.com>
2025-04-15 16:58:53 +0200

    required data annotation

--- equilog-backend/DTOs/WallPostDTOs/WallPostDto.cs
+++ equilog-backend/DTOs/WallPostDTOs/WallPostDto.cs
@@ -1,7 +1,10 @@
-﻿namespace equilog_backend.DTOs.WallPostDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.WallPostDTOs
 {
     public class WallPostDto
     {
+        [Required]
         public int StableIdFk { get; set; }
         public string? Title { get; set; }
         public string? Body { get; set; }

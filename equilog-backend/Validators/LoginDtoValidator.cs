82dcde5261c898bb72aa8d80f9e1b7af38f2e077
Anton <anton.dahlstrom@hotmail.com>
2025-04-16 14:56:37 +0900

    add loginDto validator class

---
+++ equilog-backend/Validators/LoginDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using equilog_backend.DTOs.AuthDTOs;
+using FluentValidation;
+
+namespace equilog_backend.Validators;
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+    public LoginDtoValidator()
+    {
+        RuleFor(l => l.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .MaximumLength(254).WithMessage("Email cannot exceed 254 characters.")
+            .EmailAddress().WithMessage("Not a valid email format.");
+
+        RuleFor(l => l.Password)
+            .NotEmpty().WithMessage("Please enter a password.");
+    }
+}

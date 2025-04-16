ddcb6ef7363be3db42568a387505331298eef851
NoahtlHoff <noah.hoff@outlook.com>
2025-04-16 14:08:22 +0200

    Added dtos for wallpost services

---
+++ equilog-backend/DTOs/WallPostDTOs/ClearWallPostDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.WallPostDTOs
+{
+    public class ClearWallPostDto
+    {
+        [Required]
+        public required int StableIdFk { get; set; }
+    }
+}

a1706bf0d716da3d233b5fd9922b42750f6e5974
Anton <anton.dahlstrom@hotmail.com>
2025-04-16 18:23:12 +0900

    add userCreateDtoValidator

---
+++ equilog-backend/Validators/UserCreateDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using equilog_backend.DTOs.UserDTOs;
+using FluentValidation;
+
+namespace equilog_backend.Validators
+{
+    public class UserCreateDtoValidator : AbstractValidator<UserCreateDto>
+    {
+        public UserCreateDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(50).WithMessage("First name cannot exceed 50 characters.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(50).WithMessage("Last name cannot exceed 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email address is required.")
+                .MaximumLength(254).WithMessage("Email cannot exceed 254 characters.")
+                .EmailAddress().WithMessage("A valid email address is required.");
+
+            RuleFor(x => x.PhoneNumber)
+                .MaximumLength(20).WithMessage("Phone number cannot exceed 20 characters.");
+        }
+    }
+}

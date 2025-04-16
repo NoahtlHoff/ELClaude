b71e1674694c15ece999c18d1ace8e8f79331d4a
NoahtlHoff <noah.hoff@outlook.com>
2025-04-16 17:10:00 +0200

    new wall-post dtos

---
+++ equilog-backend/DTOs/WallPostDTOs/WallPostClearDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.WallPostDTOs
+{
+    public class WallPostClearDto
+    {
+        public string? Title { get; set; } = null;
+
+        public string? Body { get; set; } = null;
+
+        public DateTime? PostDate { get; set; } = null;
+
+        public DateTime? LastEdited { get; set; } = null;
+    }
+}

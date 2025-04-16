2d0809a028522de2aef240ceebc61ab9d87f0e26
NoahtlHoff <noah.hoff@outlook.com>
2025-04-16 12:03:22 +0200

    WallPost Services

--- equilog-backend/Services/WallPostService.cs
+++ equilog-backend/Services/WallPostService.cs
@@ -4,6 +4,7 @@ using equilog_backend.Data;
 using equilog_backend.DTOs.UserDTOs;
 using equilog_backend.DTOs.WallPostDTOs;
 using equilog_backend.Interfaces;
+using equilog_backend.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
 
@@ -16,7 +17,7 @@ namespace equilog_backend.Services
             try
             {
                 var wallPost = await context.WallPosts
-                    .Where(s => s.Id == stableId)
+                    .Where(wp => wp.StableIdFk == stableId)
                     .FirstOrDefaultAsync();
 
                 if (wallPost == null)
@@ -34,19 +35,96 @@ namespace equilog_backend.Services
             }
         }
 
-        public Task<ApiResponse<WallPostDto?>> ReplaceWallPost(WallPostDto wallPostDto)
+        public async Task<ApiResponse<WallPostDto?>> ReplaceWallPost(WallPostDto wallPostDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var wallPost = await context.WallPosts
+                    .Where(wp => wp.StableIdFk == wallPostDto.StableIdFk)
+                    .FirstOrDefaultAsync();
+
+                if (wallPost == null)
+                    return ApiResponse<WallPostDto>.Failure(HttpStatusCode.NotFound,
+                    "Error: WallPost not found");
+
+                wallPost.Title = wallPostDto.Title;
+                wallPost.Body = wallPostDto.Body;
+                wallPost.PostDate = DateTime.UtcNow;
+                wallPost.LastEdited = null;
+
+                await context.SaveChangesAsync();
+
+                return ApiResponse<WallPostDto>.Success(HttpStatusCode.OK,
+                    mapper.Map<WallPostDto>(wallPost),
+                    "Posted successfully");
+            }
+            catch (Exception ex)
+            {
+                return ApiResponse<WallPostDto>.Failure(HttpStatusCode.InternalServerError,
+                    ex.Message);
+            }
         }
 
-        public Task<ApiResponse<WallPostDto?>> EditWallPost(WallPostDto wallPostDto)
+        public async Task<ApiResponse<WallPostDto?>> EditWallPost(WallPostDto wallPostDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var wallPost = await context.WallPosts
+                    .Where(wp => wp.StableIdFk == wallPostDto.StableIdFk)
+                    .FirstOrDefaultAsync();
+
+                if (wallPost == null)
+                    return ApiResponse<WallPostDto>.Failure(HttpStatusCode.NotFound,
+                    "Error: WallPost not found");
+
+                wallPost.Title = wallPostDto.Title;
+                wallPost.Body = wallPostDto.Body;
+                wallPost.LastEdited = DateTime.UtcNow;
+
+                await context.SaveChangesAsync();
+
+                return ApiResponse<WallPostDto>.Success(HttpStatusCode.OK,
+                    mapper.Map<WallPostDto>(wallPost),
+                    "Edited successfully");
+            }
+            catch (Exception ex)
+            {
+                return ApiResponse<WallPostDto>.Failure(HttpStatusCode.InternalServerError,
+                    ex.Message);
+            }
         }
 
-        public Task<ApiResponse<WallPostDto?>> ClearWallPost(WallPostDto wallPostDto)
+        public async Task<ApiResponse<WallPostDto?>> ClearWallPost(WallPostDto wallPostDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var wallPost = await context.WallPosts
+                    .Where(wp => wp.StableIdFk == wallPostDto.StableIdFk)
+                    .FirstOrDefaultAsync();
+
+                if (wallPost == null)
+                {
+                    return ApiResponse<WallPostDto>.Failure(HttpStatusCode.NotFound,
+                        "Error: Wall post not found for this stable");
+                }
+
+                wallPost.Title = null;
+                wallPost.Body = null;
+                wallPost.PostDate = null;
+                wallPost.LastEdited = null;
+                wallPost.PostDate = null;
+
+                await context.SaveChangesAsync();
+
+                return ApiResponse<WallPostDto>.Success(HttpStatusCode.OK,
+                    mapper.Map<WallPostDto>(wallPost),
+                    "Wallpost cleared successfully");
+            }
+            catch (Exception ex)
+            {
+                return ApiResponse<WallPostDto>.Failure(HttpStatusCode.InternalServerError,
+                    ex.Message);
+            }
         }
     }
 }

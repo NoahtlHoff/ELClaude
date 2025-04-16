2fc79909270fa112dca5bd7063ffdd400722fb5e
NoahtlHoff <noah.hoff@outlook.com>
2025-04-16 15:20:44 +0200

    Changed arguments to specific dtos

--- equilog-backend/Interfaces/IWallPostService.cs
+++ equilog-backend/Interfaces/IWallPostService.cs
@@ -7,10 +7,10 @@ namespace equilog_backend.Interfaces
     {
         public Task<ApiResponse<WallPostDto?>> GetWallPost(int stableId);
 
-        public Task<ApiResponse<WallPostDto?>> ReplaceWallPost(WallPostDto wallPostDto);
+        public Task<ApiResponse<WallPostDto?>> ReplaceWallPost(UpdateWallPostDto updateWallPostDto);
 
-        public Task<ApiResponse<WallPostDto?>> EditWallPost(WallPostDto wallPostDto);
+        public Task<ApiResponse<WallPostDto?>> EditWallPost(UpdateWallPostDto updateWallPostDto);
 
-        public Task<ApiResponse<WallPostDto?>> ClearWallPost(WallPostDto wallPostDto);
+        public Task<ApiResponse<WallPostDto?>> ClearWallPost(ClearWallPostDto clearWallPostDto);
     }
 }

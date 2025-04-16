9f3d3d5d8be70c94ec8d85c208f54f635eb4f852
NoahtlHoff <noah.hoff@outlook.com>
2025-04-16 10:02:26 +0200

    changed to correct prop name

--- equilog-backend/Interfaces/IWallPostService.cs
+++ equilog-backend/Interfaces/IWallPostService.cs
@@ -5,7 +5,7 @@ namespace equilog_backend.Interfaces
 {
     public interface IWallPostService
     {
-        public Task<ApiResponse<WallPostDto?>> GetWallPost(int StableId);
+        public Task<ApiResponse<WallPostDto?>> GetWallPost(int stableId);
 
         public Task<ApiResponse<WallPostDto?>> ReplaceWallPost(WallPostDto wallPostDto);
 

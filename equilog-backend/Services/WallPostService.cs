b1f9d799f6fea241db661896f9273c10eefa8e22
NoahtlHoff <noah.hoff@outlook.com>
2025-04-16 10:04:28 +0200

    Service for fetching wall

--- equilog-backend/Services/WallPostService.cs
+++ equilog-backend/Services/WallPostService.cs
@@ -1,14 +1,37 @@
-﻿using equilog_backend.Common;
+﻿using AutoMapper;
+using equilog_backend.Common;
+using equilog_backend.Data;
+using equilog_backend.DTOs.UserDTOs;
 using equilog_backend.DTOs.WallPostDTOs;
 using equilog_backend.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace equilog_backend.Services
 {
-    public class WallPostService : IWallPostService
+    public class WallPostService(EquilogDbContext context, IMapper mapper) : IWallPostService
     {
-        public Task<ApiResponse<WallPostDto?>> GetWallPost(int StableId)
+        public async Task<ApiResponse<WallPostDto?>> GetWallPost(int stableId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var wallPost = await context.WallPosts
+                    .Where(s => s.Id == stableId)
+                    .FirstOrDefaultAsync();
+
+                if (wallPost == null)
+                    return ApiResponse<WallPostDto>.Failure(HttpStatusCode.NotFound,
+                    "Error: WallPost not found");
+
+                return ApiResponse<WallPostDto>.Success(HttpStatusCode.OK,
+                    mapper.Map<WallPostDto>(wallPost),
+                    null);
+            }
+            catch (Exception ex)
+            {
+                return ApiResponse<WallPostDto>.Failure(HttpStatusCode.InternalServerError,
+                    ex.Message);
+            }
         }
 
         public Task<ApiResponse<WallPostDto?>> ReplaceWallPost(WallPostDto wallPostDto)

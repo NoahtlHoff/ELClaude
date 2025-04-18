bd9140c6ef12b968af8064b9135641deaa2ceead
NoahtlHoff <noah.hoff@outlook.com>
2025-04-18 15:00:12 +0200

    Deleted unused dto

--- equilog-backend/DTOs/WallPostDTOs/WallPostCreateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace equilog_backend.DTOs.WallPostDTOs
-{
-    public class WallPostCreateDto
-    {
-        public string? Title { get; set; } = null;
-
-        public string? Body { get; set; } = null;
-    }
-}

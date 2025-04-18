37449f66b84804e0ddfa1e709b4a474217573cc0
NoahtlHoff <noah.hoff@outlook.com>
2025-04-18 14:15:08 +0200

    Added Https status code

---
+++ equilog-backend/DTOs/WallPostDTOs/WallPostCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace equilog_backend.DTOs.WallPostDTOs
+{
+    public class WallPostCreateDto
+    {
+        public string? Title { get; set; } = null;
+
+        public string? Body { get; set; } = null;
+    }
+}

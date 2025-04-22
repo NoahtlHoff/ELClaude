d45d6cc6b12d8d140f91e92c9d2a8b1a0dbd1699
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-22 10:14:49 +0200

    Removed unnecessary method and made SendEmail more modular by making it use an interface as one of it's arguments

--- equilog-backend/Interfaces/ITwilioService.cs
+++ equilog-backend/Interfaces/ITwilioService.cs
@@ -5,7 +5,6 @@ namespace equilog_backend.Interfaces
     public interface ITwilioService
     {
         Task<ApiResponse<string?>> SendEmail (string recipientEmail, IEmail email);
-        Task<bool> SendWelcomeEmail(string toEmail);
         Task<bool> SendVerificationCode(string userEmail);
         Task<bool> VerifyVerificationCode(string userEmail, string code);
     }

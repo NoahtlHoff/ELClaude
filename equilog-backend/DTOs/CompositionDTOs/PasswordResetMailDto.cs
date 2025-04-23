0c7cf6c9459ce895e0a6dcd0b3abd842c37dfdf4
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 20:59:11 +0200

    Created composition dto for sending password reset mails

---
+++ equilog-backend/DTOs/CompositionDTOs/PasswordResetMailDto.cs
@@ -0,0 +1,10 @@
+﻿using equilog_backend.DTOs.MailTrapDTOs;
+using equilog_backend.DTOs.PasswordResetDTOs;
+
+namespace equilog_backend.DTOs.CompositionDTOs;
+
+public class PasswordResetMailDto
+{
+    public required PasswordResetRequestDto PasswordResetRequestDto { get; set; }
+    public required MailTrapPasswordResetDto MailTrapPasswordResetDto { get; set; }
+}

70449248982a59d84e9365773206d5cfa54929ac
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 20:30:15 +0200

    Added new migration

---
+++ equilog-backend/Migrations/20250423182859_PasswordReset_2.0.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace equilog_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordReset_20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ResetCode",
+                table: "PasswordResetRequests",
+                type: "nvarchar(38)",
+                maxLength: 38,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ResetCode",
+                table: "PasswordResetRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(38)",
+                oldMaxLength: 38);
+        }
+    }
+}

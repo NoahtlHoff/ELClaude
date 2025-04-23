1241cbd9c38ecc6b12979d6e19e5e6ed9d703fa5
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 20:39:51 +0200

    Added properties to dto class file

--- equilog-backend/DTOs/PasswordResetDTOs/PasswordResetRequestDto.cs
+++ equilog-backend/DTOs/PasswordResetDTOs/PasswordResetRequestDto.cs
@@ -1,6 +1,8 @@
-﻿namespace equilog_backend.DTOs.AuthDTOs;
+﻿namespace equilog_backend.DTOs.PasswordResetDTOs;
 
 public class PasswordResetRequestDto
 {
-    
+    public required string ResetCode { get; set; }
+
+    public required DateTime ExpirationDate { get; set; }
 }

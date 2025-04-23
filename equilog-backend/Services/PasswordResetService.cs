ea4d9053a92dd9065db723767e41a8fe3745dbef
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 20:24:35 +0200

    Started implementing method in password reset service

--- equilog-backend/Services/PasswordResetService.cs
+++ equilog-backend/Services/PasswordResetService.cs
@@ -1,6 +1,25 @@
-﻿namespace equilog_backend.Services;
+﻿using equilog_backend.Common;
+using equilog_backend.Data;
+using equilog_backend.DTOs.AuthDTOs;
+using equilog_backend.Models;
 
-public class PasswordResetService
+namespace equilog_backend.Services;
+
+public class PasswordResetService(EquilogDbContext context)
 {
-    
+    public async Task<ApiResponse<PasswordResetRequestDto?>> CreatePasswordResetRequest(int id)
+    {
+        try
+        {
+            var passwordResetRequest = new PasswordResetRequest()
+            {
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+    }
 }

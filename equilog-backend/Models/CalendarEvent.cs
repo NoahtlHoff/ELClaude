c94d5f98aa6680e0b88a2ec8b6a5b3db98e0545c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 12:26:38 +0200

    Added UserIdFK property to CalendarEvent model

--- equilog-backend/Models/CalendarEvent.cs
+++ equilog-backend/Models/CalendarEvent.cs
@@ -15,6 +15,13 @@ public class CalendarEvent
 
     public required DateTime EndDateTime { get; set; }
 
+    [StringLength(50)]
+    public required string Type { get; set; }
+
+    [ForeignKey("User")]
+    public required int UserIdFk { get; set; }
+    public virtual User? User { get; set; }
+
     [ForeignKey("Stable")]
     public required int StableIdFk { get; set; }
     public virtual Stable? Stable { get; set; }

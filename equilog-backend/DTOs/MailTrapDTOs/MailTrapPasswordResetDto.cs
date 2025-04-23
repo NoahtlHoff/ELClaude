d2268955d8676f2788b74bd84dbc0b195062a411
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 21:29:06 +0200

    Changed parameter in dto class

--- equilog-backend/DTOs/MailTrapDTOs/MailTrapPasswordResetDto.cs
+++ equilog-backend/DTOs/MailTrapDTOs/MailTrapPasswordResetDto.cs
@@ -1,9 +1,10 @@
-﻿using equilog_backend.Interfaces;
+﻿using equilog_backend.DTOs.PasswordResetDTOs;
+using equilog_backend.Interfaces;
 
 namespace equilog_backend.DTOs.MailTrapDTOs;
 
-public class MailTrapPasswordResetDto(string resetCode, DateTime expirationDate) : IMailTrap
+public class MailTrapPasswordResetDto(PasswordResetDto? passwordResetDto) : IMailTrap
 {
     public string Subject { get; set; } = "Equilog password reset";
-    public string Body { get; set; } = $"Hi! You have requested a password reset for your Equilog account.\nUse this code to reset your password {resetCode}.\nThe code will expire {expirationDate}\n If you did not request a password reset you can ignore this email.";
+    public string Body { get; set; } = $"Hi! You have requested a password reset for your Equilog account.\nUse this code to reset your password {passwordResetDto.ResetCode}.\nThe code will expire {passwordResetDto.ExpirationDate}\n If you did not request a password reset you can ignore this email.";
 }

9c9c067f860ac5f6633058683a82b9268e19b5df
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 20:39:11 +0200

    Added method for creating password reset requests

--- equilog-backend/Services/PasswordResetService.cs
+++ equilog-backend/Services/PasswordResetService.cs
@@ -1,11 +1,13 @@
-﻿using equilog_backend.Common;
+﻿using System.Net;
+using AutoMapper;
+using equilog_backend.Common;
 using equilog_backend.Data;
-using equilog_backend.DTOs.AuthDTOs;
+using equilog_backend.DTOs.PasswordResetDTOs;
 using equilog_backend.Models;
 
 namespace equilog_backend.Services;
 
-public class PasswordResetService(EquilogDbContext context)
+public class PasswordResetService(EquilogDbContext context, IMapper mapper)
 {
     public async Task<ApiResponse<PasswordResetRequestDto?>> CreatePasswordResetRequest(int id)
     {
@@ -13,13 +15,22 @@ public class PasswordResetService(EquilogDbContext context)
         {
             var passwordResetRequest = new PasswordResetRequest()
             {
-                
-            }
+                UserIdFk = id,
+                ResetCode = Generate.PasswordResetCode(),
+                ExpirationDate = DateTime.Now.AddHours(24)
+            };
+
+            context.PasswordResetRequests.Add(passwordResetRequest);
+            await context.SaveChangesAsync();
+            
+            return ApiResponse<PasswordResetRequestDto?>.Success(HttpStatusCode.Created,
+                mapper.Map<PasswordResetRequestDto>(passwordResetRequest),
+                "Password reset request was created successfully");
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
-            throw;
+            return ApiResponse<PasswordResetRequestDto?>.Failure(HttpStatusCode.InternalServerError,
+                ex.Message);
         }
     }
 }

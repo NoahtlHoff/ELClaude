d36f7f1264ce5393d58fa9e966ab78f72c0d3a22
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-23 20:54:46 +0200

    Created dto for password reset mail

---
+++ equilog-backend/DTOs/MailTrapDTOs/MailTrapPasswordResetDto.cs
@@ -0,0 +1,9 @@
+﻿using equilog_backend.Interfaces;
+
+namespace equilog_backend.DTOs.MailTrapDTOs;
+
+public class MailTrapPasswordResetDto(string resetCode, DateTime expirationDate) : IMailTrap
+{
+    public string Subject { get; set; } = "Equilog password reset";
+    public string Body { get; set; } = $"Hi! You have requested a password reset for your Equilog account.\nUse this code to reset your password {resetCode}.\nThe code will expire {expirationDate}\n If you did not request a password reset you can ignore this email.";
+}

6e521dcda5b7588e2c1ea072e1008eb82886992e
Anton <anton.dahlstrom@hotmail.com>
2025-04-24 20:28:28 +0900

    remove data annotations from userUpdateDto

--- equilog-backend/DTOs/UserDTOs/UserUpdateDto.cs
+++ equilog-backend/DTOs/UserDTOs/UserUpdateDto.cs
@@ -1,16 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace equilog_backend.DTOs.UserDTOs
+﻿namespace equilog_backend.DTOs.UserDTOs
 {
     public class UserUpdateDto
     {
-        [Required]
         public required int Id { get; set; }
 
-        [Required]
         public required string FirstName { get; set; }
 
-        [Required]
         public required string LastName { get; set; }
     }
 }

36151d65c35f09ea69b553d6ddb04a91af22de8f
Anton <anton.dahlstrom@hotmail.com>
2025-04-24 20:10:24 +0900

    add validation for horseUpdateDto

---
+++ equilog-backend/Validators/HorseUpdateDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using equilog_backend.DTOs.HorseDTOs;
+using FluentValidation;
+
+namespace equilog_backend.Validators;
+public class HorseUpdateDtoValidator : AbstractValidator<HorseUpdateDto>
+{
+    public HorseUpdateDtoValidator()
+    {
+        RuleFor(e => e.Id)
+            .NotEmpty()
+            .GreaterThan(0).WithMessage("Horse ID must be greater than 0.");
+
+        RuleFor(h => h.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+
+        RuleFor(h => h.Age)
+            .Must(age => age == null || age < DateOnly.FromDateTime(DateTime.Today))
+            .WithMessage("Age must be a date in the past.");
+
+        RuleFor(h => h.Color)
+            .MaximumLength(50).When(h => h.Color != null);
+
+        RuleFor(h => h.Breed)
+            .MaximumLength(50).When(h => h.Breed != null);
+    }
+}

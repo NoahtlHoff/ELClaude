c0bea64b99e1c3e515fd9f547b60b46913fa32f7
Anton <anton.dahlstrom@hotmail.com>
2025-04-24 20:13:07 +0900

    remove data annotations from stableUpdateDto

--- equilog-backend/DTOs/StableDTOs/StableUpdateDto.cs
+++ equilog-backend/DTOs/StableDTOs/StableUpdateDto.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace equilog_backend.DTOs.StableDTOs;
+﻿namespace equilog_backend.DTOs.StableDTOs;
 
 public class StableUpdateDto
 {
-    [Required]
     public required int Id { get; set; }
 
-    [Required]
     public required string Name { get; set; }
 }

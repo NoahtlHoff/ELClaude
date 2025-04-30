5137fe1dc202b4ffa8557aa997332ec0cda74cdf
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-04-30 14:53:54 +0200

    Created dto class for password reset that requires token

---
+++ equilog-backend/DTOs/PasswordResetDTOs/PasswordResetWithTokenDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.PasswordResetDTOs;
+
+public class PasswordResetWithTokenDto
+{
+    [Required]
+    public required string Token { get; set; }
+    
+    [Required]
+    [StringLength(100, MinimumLength = 8)]
+    public required string NewPassword { get; set; }
+    
+    [Required]
+    [StringLength(100, MinimumLength = 8)]
+    public required string ConfirmPassword { get; set; }
+}

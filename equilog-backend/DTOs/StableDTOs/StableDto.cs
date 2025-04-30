3e07ad2ea836d81b13fc43f70ce7915294e0620c
NoahtlHoff <noah.hoff@outlook.com>
2025-04-30 17:23:29 +0200

    match dto with new model

--- equilog-backend/DTOs/StableDTOs/StableDto.cs
+++ equilog-backend/DTOs/StableDTOs/StableDto.cs
@@ -5,4 +5,6 @@ public class StableDto
     public required int Id { get; set; }
 
     public required string Name { get; set; }
+    public required string Type { get; set; }
+    public required string County { get; set; }
 }

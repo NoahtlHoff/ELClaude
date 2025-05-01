9b2c26ead346e4aa38bd7e6b2b5988a3bd5e2cf9
Anton <anton.dahlstrom@hotmail.com>
2025-05-01 15:38:07 +0900

    remove data annotations from passwordResetDto

--- equilog-backend/DTOs/PasswordResetDTOs/PasswordResetDto.cs
+++ equilog-backend/DTOs/PasswordResetDTOs/PasswordResetDto.cs
@@ -1,15 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace equilog_backend.DTOs.PasswordResetDTOs;
+﻿namespace equilog_backend.DTOs.PasswordResetDTOs;
 
 public class PasswordResetDto
 {
-    [EmailAddress]
     public required string Email { get; set; }
-    
-    [StringLength(100, MinimumLength = 8)]
+
     public required string NewPassword { get; set; }
-    
-    [StringLength(100, MinimumLength = 8)]
+
     public required string ConfirmPassword { get; set; }
 }

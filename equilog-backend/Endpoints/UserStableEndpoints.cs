140731923056e90998fccf2f9be5b475bae9d12a
NoahtlHoff <noah.hoff@outlook.com>
2025-05-02 00:14:35 +0200

    Endpoint for getting stables with roles for a user

---
+++ equilog-backend/Endpoints/UserStableEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using equilog_backend.Common;
+using equilog_backend.Interfaces;
+
+namespace equilog_backend.Endpoints
+{
+    public class UserStableEndpoints
+    {
+        public static void RegisterEndpoints(WebApplication app)
+        {
+            app.MapGet("/api/userstables/{id:int}", GetUserStable)
+                .WithName("GetUserStable");
+        }
+        private static async Task<IResult> GetUserStable(IUserService userService)
+        {
+            return Result.Generate(await userService.GetUserStableAsync());
+        }
+    }
+}

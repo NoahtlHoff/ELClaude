5a2c5ebcc7f145a1f84f015af447dee33aa564fe
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 17:24:01 +0200

    Added StableJoinRequests db set to Data class

--- equilog-backend/Data/EquilogDbContext.cs
+++ equilog-backend/Data/EquilogDbContext.cs
@@ -29,4 +29,6 @@ public class EquilogDbContext(DbContextOptions<EquilogDbContext> options)
     public DbSet<PasswordResetRequest> PasswordResetRequests { get; set; }
     
     public DbSet<RefreshToken> RefreshTokens { get; set; }
+    
+    public DbSet<StableJoinRequest> StableJoinRequests { get; set; }
 }

b8f1c37c1d624af0f5ef3f5ae2a326746e75a95d
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 19:53:10 +0200

    Fixed code formating

--- equilog-backend/Common/MappingProfile.cs
+++ equilog-backend/Common/MappingProfile.cs
@@ -33,8 +33,10 @@ namespace equilog_backend.Common
             CreateMap<StableUpdateDto, Stable>(MemberList.Source);
 
             CreateMap<StablePost, StablePostDto>()
-                .ForMember(dest => dest.PosterFirstName, opt => opt.MapFrom(src => src.User != null ? src.User.FirstName : null))
-                .ForMember(dest => dest.PosterLastName, opt => opt.MapFrom(src => src.User != null ? src.User.LastName : null))
+                .ForMember(dest => dest.PosterFirstName, opt 
+                    => opt.MapFrom(src => src.User != null ? src.User.FirstName : null))
+                .ForMember(dest => dest.PosterLastName, opt 
+                    => opt.MapFrom(src => src.User != null ? src.User.LastName : null))
                 .ReverseMap();
             CreateMap<StablePostCreateDto, StablePost>(MemberList.Source);
             CreateMap<StablePostUpdateDto, StablePost>(MemberList.Source);

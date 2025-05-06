fbd0e141d215f0f96b730bf6f4d63a737d2e954a
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 19:30:14 +0200

    Created dto

---
+++ equilog-backend/DTOs/StableJoinRequestDTOs/StableJoinRequestResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace equilog_backend.DTOs.StableJoinRequestDTOs;
+
+public class StableJoinRequestResponseDto
+{
+    public required string Email { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+}

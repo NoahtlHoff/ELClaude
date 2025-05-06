cb9b07d0074d9e65749bb5875fb7e8724623bba8
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 17:44:01 +0200

    Created enpoints for creating stable join request

---
+++ equilog-backend/Endpoints/StableJoinRequestEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using equilog_backend.Common;
+using equilog_backend.DTOs.StableJoinRequestDTOs;
+using equilog_backend.Interfaces;
+
+namespace equilog_backend.Endpoints;
+
+public class StableJoinRequestEndpoints
+{
+    public static void RegisterEndpoints(WebApplication app)
+    {
+        app.MapPost("/api/create-stable-join-request", CreateStableJoinRequest)
+            .WithName("CreateStableJoinRequest");
+    }
+
+    private static async Task<IResult> CreateStableJoinRequest(
+        IStableJoinRequestService stableJoinRequestService,
+        StableJoinRequestDto stableJoinRequestDto)
+    {
+        return Result.Generate(await stableJoinRequestService.CreateStableJoinRequest(stableJoinRequestDto));
+    }
+}

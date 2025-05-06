157104b013258b2e6bc5ef62512499ac8e63dc8b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 20:37:50 +0200

    Added endpoints for stable invites

---
+++ equilog-backend/Endpoints/StableInviteEndpoints.cs
@@ -0,0 +1,51 @@
+﻿using equilog_backend.Common;
+using equilog_backend.DTOs.StableInviteDTOs;
+using equilog_backend.Interfaces;
+
+namespace equilog_backend.Endpoints;
+
+public class StableInviteEndpoints
+{
+    public static void RegisterEndpoints(WebApplication app)
+    {
+        app.MapGet("/api/get-stable-invite-by-stable/{id:int}", GetStableInviteByStableId)
+            .WithName("GetStableInviteByStableId");
+
+        app.MapPost("/api/create-stable-invite", CreateStableInvite)
+            .WithName("CreateStableInvite");
+
+        app.MapPost("/api/accept-stable-invite", AcceptStableInvite)
+            .WithName("AcceptStableInvite");
+
+        app.MapPost("/api/refuse-stable-invite", RefuseStableInvite)
+            .WithName("RefuseStableInvite");
+    }
+
+    private static async Task<IResult> GetStableInviteByStableId(
+        IStableInviteService stableInviteService,
+        int id)
+    {
+        return Result.Generate(await stableInviteService.GetStableInviteByStableIdAsync(id));
+    }
+
+    private static async Task<IResult> CreateStableInvite(
+        IStableInviteService stableInviteService,
+        StableInviteDto stableInviteDto)
+    {
+        return Result.Generate(await stableInviteService.CreateStableInviteAsync(stableInviteDto));
+    }
+
+    private static async Task<IResult> AcceptStableInvite(
+        IStableInviteService stableInviteService,
+        StableInviteDto stableInviteDto)
+    {
+        return Result.Generate(await stableInviteService.AcceptStableInviteAsync(stableInviteDto));
+    }
+
+    private static async Task<IResult> RefuseStableInvite(
+        IStableInviteService stableInviteService,
+        StableInviteDto stableInviteDto)
+    {
+        return Result.Generate(await stableInviteService.RefuseStableInviteAsync(stableInviteDto));
+    }
+}

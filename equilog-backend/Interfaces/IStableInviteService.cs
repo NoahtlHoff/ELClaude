e33bbb1fd89c3b3d046f3480fecb985bb3b7a2ef
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 20:08:12 +0200

    Added method to interface and created dto class

--- equilog-backend/Interfaces/IStableInviteService.cs
+++ equilog-backend/Interfaces/IStableInviteService.cs
@@ -1,6 +1,9 @@
-﻿namespace equilog_backend.Interfaces;
+﻿using equilog_backend.Common;
+using equilog_backend.DTOs.StableInviteDTOs;
+
+namespace equilog_backend.Interfaces;
 
 public interface IStableInviteService
 {
-    
+    Task<ApiResponse<Unit>> CreateStableInviteAsync(StableInviteDto stableInviteDto);
 }

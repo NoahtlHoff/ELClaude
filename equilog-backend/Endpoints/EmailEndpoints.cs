88b91b7adda3ac8dd3017bb4c71a8a6b9dab709c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 12:17:27 +0200

    Moved endpoint

--- equilog-backend/Endpoints/EmailEndpoints.cs
+++ equilog-backend/Endpoints/EmailEndpoints.cs
@@ -13,10 +13,23 @@ public class EmailEndpoints
         app.MapPost("/api/email-send/welcome/", SendEmail)
             .AddEndpointFilter<ValidationFilter<EmailDto>>()
             .WithName("SendTwilioMail");
+        
+        // -- Endpoints for compositions --
+        
+        // Send password reset email.
+        app.MapPost("/api/password-reset-email/send", SendPasswordResetEmail)
+            .AddEndpointFilter<ValidationFilter<EmailDto>>()
+            .WithName("SendPasswordResetEmail");
     }
 
     private static async Task<IResult> SendEmail(IEmailService emailService, EmailDto emailDto)
     {
         return Result.Generate(await emailService.SendEmailAsync(new EmailSendWelcomeDto(), emailDto.Email));
+    }
+    
+    // -- Result generators for compositions --
+    private static async Task<IResult> SendPasswordResetEmail(IPasswordResetComposition passwordResetComposition, EmailDto emailDto)
+    {
+        return Result.Generate(await passwordResetComposition.SendPasswordResetEmailAsync(emailDto.Email));
     }
 }

1a9ef744d76c5c71ba3ae3ae5e6dc858754a4040
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 12:27:47 +0200

    Fixed spacing between properties

--- equilog-backend/DTOs/StableDTOs/StableDto.cs
+++ equilog-backend/DTOs/StableDTOs/StableDto.cs
@@ -3,7 +3,10 @@ namespace equilog_backend.DTOs.StableDTOs;
 public class StableDto
 {
     public required int Id { get; init; }
+    
     public required string Name { get; set; }
+    
     public required string Type { get; set; }
+    
     public required string County { get; set; }
 }
--- equilog-backend/DTOs/StableDTOs/StableSearchDto.cs
+++ equilog-backend/DTOs/StableDTOs/StableSearchDto.cs
@@ -5,8 +5,11 @@ namespace equilog_backend.DTOs.StableDTOs
     public class StableSearchDto
     {
         public int Id { get; set; }
+        
         public required string Name { get; set; }
+        
         public string? Address { get; set; } = null;
+        
         public required string County { get; set; }
     }
 }

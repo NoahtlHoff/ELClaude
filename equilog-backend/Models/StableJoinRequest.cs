115fd029f5767118874f1f3ca1fe725fb2fb3630
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-06 17:06:46 +0200

    Created model for stable join requests

---
+++ equilog-backend/Models/StableJoinRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.Models;
+
+public class StableJoinRequest
+{
+    [Key]
+    public int Id { get; set; }
+
+    public required int UserIdFk { get; set; }
+
+    public required int StableIdFk { get; set; }
+
+    public required bool Accepted { get; set; }
+}

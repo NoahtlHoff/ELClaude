0f6c097240b1bcb5aa13b92c9b20399359fde819
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-12 15:10:51 +0200

    Created method to create a connection between a horse and a stable

--- equilog-backend/Interfaces/IStableHorseService.cs
+++ equilog-backend/Interfaces/IStableHorseService.cs
@@ -1,6 +1,8 @@
-﻿namespace equilog_backend.Interfaces;
+﻿using equilog_backend.Common;
+
+namespace equilog_backend.Interfaces;
 
 public interface IStableHorseService
 {
-    
+    Task<ApiResponse<Unit>> CreateStableHorseConnectionAsync(int stableId, int horseId);
 }
--- equilog-backend/Services/StableHorseService.cs
+++ equilog-backend/Services/StableHorseService.cs
@@ -1,8 +1,34 @@
-﻿using equilog_backend.Interfaces;
+﻿using System.Net;
+using equilog_backend.Common;
+using equilog_backend.Data;
+using equilog_backend.Interfaces;
+using equilog_backend.Models;
 
 namespace equilog_backend.Services;
 
-public class StableHorseService : IStableHorseService
+public class StableHorseService(EquilogDbContext context) : IStableHorseService
 {
-    
+    public async Task<ApiResponse<Unit>> CreateStableHorseConnectionAsync(int stableId, int horseId)
+    {
+        try
+        {
+            var stableHorse = new StableHorse
+            {
+                StableIdFk = stableId,
+                HorseIdFk = horseId
+            };
+
+            context.StableHorses.Add(stableHorse);
+            await context.SaveChangesAsync();
+        
+            return ApiResponse<Unit>.Success(HttpStatusCode.Created,
+                Unit.Value,
+                null);
+        }
+        catch (Exception ex)
+        {
+            return ApiResponse<Unit>.Failure(HttpStatusCode.InternalServerError,
+                ex.Message);
+        }
+    }
 }

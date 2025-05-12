934a9c96dd8f699e689a383b451d374781b0b848
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-12 15:55:37 +0200

    Created endpoint file for stable horses and added one endpoint

---
+++ equilog-backend/Endpoints/StableHorseEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using equilog_backend.Common;
+using equilog_backend.Interfaces;
+
+namespace equilog_backend.Endpoints;
+
+public class StableHorseEndpoints
+{
+    public static void RegisterEndpoints(WebApplication app)
+    {
+        app.MapGet("/api/stable-horses", GetStableHorses)
+            .WithName("GetStableHorses");
+    }
+
+    private static async Task<IResult> GetStableHorses(
+        IStableHorseService stableHorseService,
+        int id)
+    {
+        return Result.Generate(await stableHorseService.GetStableHorsesAsync(id));
+    }
+}

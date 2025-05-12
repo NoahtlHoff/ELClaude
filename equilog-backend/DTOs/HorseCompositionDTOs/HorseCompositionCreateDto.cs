a374cf3877b05a19d8f81a70476d823076580c7d
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-12 13:37:34 +0200

    Added properties to new dto

--- equilog-backend/DTOs/HorseCompositionDTOs/HorseCompositionCreateDto.cs
+++ equilog-backend/DTOs/HorseCompositionDTOs/HorseCompositionCreateDto.cs
@@ -2,5 +2,15 @@
 
 public class HorseCompositionCreateDto
 {
+    public required int UserId { get; init; }
     
+    public required int StableId { get; set; }
+    
+    public required string Name { get; init; }
+
+    public DateOnly? Age { get; init; }
+
+    public string? Color { get; init; }
+
+    public string? Breed { get; init; }
 }

52e97d784526d598105b58a36283556df2b0642e
Anton <anton.dahlstrom@hotmail.com>
2025-05-13 14:59:34 +0200

    add horseCompositionCreateDtoValidator

---
+++ equilog-backend/Validators/HorseCompositionCreateDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using equilog_backend.DTOs.HorseCompositionDTOs;
+using FluentValidation;
+
+namespace equilog_backend.Validators
+{
+    public class HorseCompositionCreateDtoValidator : AbstractValidator<HorseCompositionCreateDto>
+    {
+        public HorseCompositionCreateDtoValidator()
+        {
+            RuleFor(e => e.UserId)
+                .NotEmpty()
+                .GreaterThan(0).WithMessage("User ID must be greater than 0.");
+
+            RuleFor(e => e.StableId)
+                .NotEmpty()
+                .GreaterThan(0).WithMessage("Stable ID must be greater than 0.");
+
+            RuleFor(x => x.Horse)
+                .SetValidator(new HorseCreateDtoValidator());
+        }
+    }
+}

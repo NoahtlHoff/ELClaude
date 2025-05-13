bf83aa05b41ee5f47a14e18ce8caf080ca80a7c5
Anton <anton.dahlstrom@hotmail.com>
2025-05-13 13:34:20 +0200

    change postcode to string

--- equilog-backend/DTOs/StableDTOs/StableDto.cs
+++ equilog-backend/DTOs/StableDTOs/StableDto.cs
@@ -3,20 +3,20 @@ namespace equilog_backend.DTOs.StableDTOs;
 public class StableDto
 {
     public required int Id { get; init; }
-    
+
     public required string Name { get; init; }
-    
+
     public required string Type { get; init; }
-    
+
     public required string Address { get; init; }
-    
+
     public required string County { get; init; }
-    
-    public required int PostCode { get; init; }
-    
+
+    public required string PostCode { get; init; }
+
     public required int MemberCount { get; init; }
-    
+
     public required int HorseCount { get; init; }
-    
+
     public required int BoxCount { get; init; }
 }

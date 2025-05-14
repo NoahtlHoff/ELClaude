c38c6f1d0c3be8c5207bb5114ee3994ff117f035
NoahtlHoff <noah.hoff@outlook.com>
2025-05-14 16:42:43 +0200

    Added dto for fetching userinfo for profile

---
+++ equilog-backend/DTOs/UserDTOs/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿using equilog_backend.DTOs.StableHorseDTOs;
+using equilog_backend.DTOs.UserHorseDTOs;
+using equilog_backend.DTOs.UserStableDTOs;
+using equilog_backend.Models;
+
+namespace equilog_backend.DTOs.UserDTOs
+{
+    public class UserProfileDto
+    {
+        public required UserStableRoleDto UserStableRole { get; init; }
+        public List<UserHorseRoleDto>? UserHorses { get; init; }
+    }
+}

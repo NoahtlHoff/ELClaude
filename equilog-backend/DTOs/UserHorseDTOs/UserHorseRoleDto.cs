6d4dabe7a222d959c5822d51fb099d1692c68f5a
NoahtlHoff <noah.hoff@outlook.com>
2025-05-14 16:43:13 +0200

    Added dto for fetching horses connected to user

---
+++ equilog-backend/DTOs/UserHorseDTOs/UserHorseRoleDto.cs
@@ -0,0 +1,13 @@
+﻿using equilog_backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace equilog_backend.DTOs.UserHorseDTOs
+{
+    public class UserHorseRoleDto
+    {
+        public required Horse Horse { get; set; }
+
+        public required int UserRole { get; set; }
+    }
+}

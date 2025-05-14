345835e43a0bae701bbacdaa316121f828947a9f
Anton <anton.dahlstrom@hotmail.com>
2025-05-14 13:15:26 +0200

    add stableLocation model

---
+++ equilog-backend/Models/StableLocation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace equilog_backend.Models
+{
+    [Table("StableLocation")]
+    public class StableLocation
+    {
+        [Key]
+        public required string PostCode { get; set; }
+        public required string City { get; set; }
+        public required string MunicipalityName { get; set; }
+        public required string MunicipalityCode { get; set; }
+        public required string CountyName { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public required string GoogleMaps { get; set; }
+    }
+}

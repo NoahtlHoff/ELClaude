fb2f2bf4ed3048d303de42ab058e2f505c6b0f34
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-05-15 01:19:44 +0200

    remove unused imports

--- equilog-backend/DTOs/UserDTOs/UserProfileDto.cs
+++ equilog-backend/DTOs/UserDTOs/UserProfileDto.cs
@@ -1,13 +1,11 @@
-﻿using equilog_backend.DTOs.StableHorseDTOs;
-using equilog_backend.DTOs.UserHorseDTOs;
+﻿using equilog_backend.DTOs.UserHorseDTOs;
 using equilog_backend.DTOs.UserStableDTOs;
-using equilog_backend.Models;
 
 namespace equilog_backend.DTOs.UserDTOs
 {
-    public class UserProfileDto
-    {
-        public required UserStableRoleDto UserStableRole { get; init; }
-        public List<UserHorseRoleDto>? UserHorses { get; init; }
-    }
+	public class UserProfileDto
+	{
+		public required UserStableRoleDto UserStableRole { get; init; }
+		public List<UserHorseRoleDto>? UserHorses { get; init; }
+	}
 }

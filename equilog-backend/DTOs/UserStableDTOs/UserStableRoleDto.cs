f5e61ed0237f3b7681536216b79233e852479b0b
NoahtlHoff <noah.hoff@outlook.com>
2025-05-14 16:43:56 +0200

    Added dto that fetches users, and their role in current stable

---
+++ equilog-backend/DTOs/UserStableDTOs/UserStableRoleDto.cs
@@ -0,0 +1,11 @@
+﻿using equilog_backend.Models;
+
+namespace equilog_backend.DTOs.UserStableDTOs
+{
+    public class UserStableRoleDto
+    {
+        public required User User { get; set; }
+
+        public required int Role { get; set; }
+    }
+}

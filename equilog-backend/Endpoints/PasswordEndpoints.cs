5eb6d8d37ef0f17330d00771db99c5a62c62cc0d
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-05-15 10:12:45 +0200

    add endpointFilter for password Dtos

--- equilog-backend/Endpoints/PasswordEndpoints.cs
+++ equilog-backend/Endpoints/PasswordEndpoints.cs
@@ -6,28 +6,30 @@ namespace equilog_backend.Endpoints;
 
 public class PasswordEndpoints
 {
-    public static void RegisterEndpoints(WebApplication app)
-    {
-        // Reset password.
-        app.MapPost("/api/reset-password", ResetPassword)
-            .WithName("RestPassword");
-        
-        // Change password.
-        app.MapPost("/api/change-password", ChangePassword)
-            .WithName("ChangePassword");
-    }
-    
-    private static async Task<IResult> ResetPassword(
-        IPasswordService passwordService,
-        PasswordResetDto passwordResetDto)
-    {
-        return Result.Generate(await passwordService.ResetPasswordAsync(passwordResetDto));
-    }
+	public static void RegisterEndpoints(WebApplication app)
+	{
+		// Reset password.
+		app.MapPost("/api/reset-password", ResetPassword)
+			.AddEndpointFilter<ValidationFilter<PasswordResetDto>>()
+			.WithName("RestPassword");
 
-    private static async Task<IResult> ChangePassword(
-        IPasswordService passwordService,
-        PasswordChangeDto passwordChangeDto)
-    {
-        return Result.Generate(await passwordService.ChangePasswordAsync(passwordChangeDto));
-    }
+		// Change password.
+		app.MapPost("/api/change-password", ChangePassword)
+			.AddEndpointFilter<ValidationFilter<PasswordChangeDto>>()
+			.WithName("ChangePassword");
+	}
+
+	private static async Task<IResult> ResetPassword(
+		IPasswordService passwordService,
+		PasswordResetDto passwordResetDto)
+	{
+		return Result.Generate(await passwordService.ResetPasswordAsync(passwordResetDto));
+	}
+
+	private static async Task<IResult> ChangePassword(
+		IPasswordService passwordService,
+		PasswordChangeDto passwordChangeDto)
+	{
+		return Result.Generate(await passwordService.ChangePasswordAsync(passwordChangeDto));
+	}
 }

8fb00af315323faf577118175c23f7cf956efee2
Anton <anton.dahlstrom@hotmail.com>
2025-05-15 14:09:04 +0200

    change back Role property name

--- equilog-backend/Models/UserStable.cs
+++ equilog-backend/Models/UserStable.cs
@@ -5,16 +5,16 @@ namespace equilog_backend.Models;
 
 public class UserStable
 {
-	[Key]
-	public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-	[ForeignKey("User")]
-	public required int UserIdFk { get; set; }
-	public virtual User? User { get; set; }
+    [ForeignKey("User")]
+    public required int UserIdFk { get; set; }
+    public virtual User? User { get; set; }
 
-	[ForeignKey("Stable")]
-	public required int StableIdFk { get; set; }
-	public virtual Stable? Stable { get; set; }
+    [ForeignKey("Stable")]
+    public required int StableIdFk { get; set; }
+    public virtual Stable? Stable { get; set; }
 
-	public required int UserRole { get; set; } // 0 = Master admin | 1 = User admin | 2 = user.
+    public required int Role { get; set; } // 0 = Master admin | 1 = User admin | 2 = user.
 }

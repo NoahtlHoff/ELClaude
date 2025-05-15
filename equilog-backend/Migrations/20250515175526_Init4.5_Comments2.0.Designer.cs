5edf2a92e7f571ffbe32873bb0ddc4d6aebc69ed
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-15 19:59:48 +0200

    New migration

--- equilog-backend/Migrations/20250515175526_Init4.5_Comments2.0.Designer.cs
+++ equilog-backend/Migrations/20250515175526_Init4.5_Comments2.0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using equilog_backend.Data;
 
@@ -11,9 +12,11 @@ using equilog_backend.Data;
 namespace equilog_backend.Migrations
 {
     [DbContext(typeof(EquilogDbContext))]
-    partial class EquilogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250515175526_Init4.5_Comments2.0")]
+    partial class Init45_Comments20
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

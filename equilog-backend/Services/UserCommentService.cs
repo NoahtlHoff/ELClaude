92e6e15ca56e910e76785d23d880928183278aea
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-15 10:28:37 +0200

    Created method to create relation between user and comment

--- equilog-backend/Services/UserCommentService.cs
+++ equilog-backend/Services/UserCommentService.cs
@@ -1,9 +1,35 @@
-﻿using equilog_backend.Data;
+﻿using System.Net;
+using equilog_backend.Common;
+using equilog_backend.Data;
 using equilog_backend.Interfaces;
+using equilog_backend.Models;
 
 namespace equilog_backend.Services;
 
 public class UserCommentService(EquilogDbContext context) : IUserCommentService
 {
-    
+    public async Task<ApiResponse<int>> CreateUserCommentConnectionAsync(int userId, int commentId)
+    {
+        try
+        {
+            var userComment = new UserComment
+            {
+                UserIdFk = userId,
+                CommentIdFk = commentId
+            };
+
+            context.UserComments.Add(userComment);
+            await context.SaveChangesAsync();
+            
+            return ApiResponse<int>.Success(HttpStatusCode.Created,
+                userComment.Id,
+                null);
+
+        }
+        catch (Exception ex)
+        {
+            return ApiResponse<int>.Failure(HttpStatusCode.InternalServerError,
+                ex.Message);
+        }
+    }
 }

e20d742e110963b7565d14b1b5e82fb229645be1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-15 12:47:14 +0200

    Created method that removes connection between user and comment

--- equilog-backend/Services/UserCommentService.cs
+++ equilog-backend/Services/UserCommentService.cs
@@ -3,6 +3,7 @@ using equilog_backend.Common;
 using equilog_backend.Data;
 using equilog_backend.Interfaces;
 using equilog_backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace equilog_backend.Services;
 
@@ -24,11 +25,36 @@ public class UserCommentService(EquilogDbContext context) : IUserCommentService
             return ApiResponse<int>.Success(HttpStatusCode.Created,
                 userComment.Id,
                 null);
-
         }
         catch (Exception ex)
         {
             return ApiResponse<int>.Failure(HttpStatusCode.InternalServerError,
+                ex.Message);
+        }
+    }
+
+    public async Task<ApiResponse<Unit>> RemoveUserCommentConnection(int userCommentId)
+    {
+        try
+        {
+            var userComment = await context.UserComments
+                .Where(uc => uc.Id == userCommentId)
+                .FirstOrDefaultAsync();
+            
+            if (userComment == null)
+                return ApiResponse<Unit>.Failure(HttpStatusCode.NotFound,
+                    "Error: Connection between user and comment not found.");
+            
+            context.UserComments.Remove(userComment);
+            await context.SaveChangesAsync();
+            
+            return ApiResponse<Unit>.Success(HttpStatusCode.OK,
+                Unit.Value,
+                "Connection between user and comment was removed successfully.");
+        }
+        catch (Exception ex)
+        {
+            return ApiResponse<Unit>.Failure(HttpStatusCode.InternalServerError,
                 ex.Message);
         }
     }

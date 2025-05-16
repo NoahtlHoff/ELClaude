e14365e84ac6c8b9585e49f582fb5f62688b7ba2
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-05-16 14:22:10 +0200

    Created composition that deletes user and handles transfer of ownership

--- equilog-backend/Compositions/UserComposition.cs
+++ equilog-backend/Compositions/UserComposition.cs
@@ -1,13 +1,41 @@
-﻿using equilog_backend.Common;
+﻿using System.Net;
+using equilog_backend.Common;
 using equilog_backend.DTOs.UserDTOs;
 using equilog_backend.Interfaces;
 
 namespace equilog_backend.Compositions;
 
-public class UserComposition : IUserComposition
+public class UserComposition(
+    IUserStableService userStableService,
+    IUserService userService) : IUserComposition
 {
     public async Task<ApiResponse<Unit>> DeleteUserCompositionAsync(UserDeleteDto userDeleteDto)
     {
-        throw new NotImplementedException();
+        var userId = userDeleteDto.UserId;
+        var stableId = userDeleteDto.StableId;
+
+        var checkOwners = await userStableService.CheckNumberOfStableOwners(userId, stableId);
+
+        if (checkOwners is { StatusCode: HttpStatusCode.InternalServerError })
+            return ApiResponse<Unit>.Failure(checkOwners.StatusCode,
+                checkOwners.Message);
+
+        if (checkOwners.IsSuccess)
+        {
+            await userService.DeleteUserAsync(userId);
+            return ApiResponse<Unit>.Success(HttpStatusCode.OK,
+                Unit.Value,
+                null);            
+        }
+
+        var setRole = await userStableService.SetRoleToOwner(stableId);
+        
+        if (setRole is { StatusCode: HttpStatusCode.InternalServerError })
+            return  ApiResponse<Unit>.Failure(setRole.StatusCode,
+                setRole.Message);
+        
+        return ApiResponse<Unit>.Success(HttpStatusCode.OK,
+            Unit.Value,
+            null);
     }
 }

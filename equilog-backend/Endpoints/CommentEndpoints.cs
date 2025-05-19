275ac84dd8c2f97fc79390cf13d575adf90dbcf9
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-05-20 00:52:37 +0200

    add validators to endpoint

--- equilog-backend/Endpoints/CommentEndpoints.cs
+++ equilog-backend/Endpoints/CommentEndpoints.cs
@@ -7,53 +7,55 @@ namespace equilog_backend.Endpoints;
 
 public class CommentEndpoints
 {
-    public static void RegisterEndpoints(WebApplication app)
-    {
-        // Get comments by StablePost id.
-        app.MapGet("/api/comment/{id:int}", GetCommentsByStablePostId)
-            .WithName("GetCommentByStableId");
-        
-        // Create comment.
-        app.MapPost("/api/comment/create", CreateComment)
-            .WithName("CreateComment");
-        
-        // Delete comment.
-        app.MapDelete("/api/comment/delete/{id:int}", DeleteComment)
-            .WithName("DeleteComment");
-        
-        // -- Endpoints for compositions --
-        
-        // create a comment with required components and relations.
-        app.MapPost("/api/comment/create/composition", CreateCommentComposition)
-            .WithName("CreateCommentComposition");
-    }
+	public static void RegisterEndpoints(WebApplication app)
+	{
+		// Get comments by StablePost id.
+		app.MapGet("/api/comment/{id:int}", GetCommentsByStablePostId)
+			.WithName("GetCommentByStableId");
 
-    private static async Task<IResult> GetCommentsByStablePostId(
-        ICommentService commentService,
-        int id)
-    {
-        return Result.Generate(await commentService.GetCommentByStablePostId(id));
-    }
+		// Create comment.
+		app.MapPost("/api/comment/create", CreateComment)
+			.AddEndpointFilter<ValidationFilter<CommentCreateDto>>()
+			.WithName("CreateComment");
 
-    private static async Task<IResult> CreateComment(
-        ICommentService commentService,
-        CommentCreateDto commentCreateDto)
-    {
-        return Result.Generate(await commentService.CreateCommentAsync(commentCreateDto));
-    }
+		// Delete comment.
+		app.MapDelete("/api/comment/delete/{id:int}", DeleteComment)
+			.WithName("DeleteComment");
 
-    private static async Task<IResult> DeleteComment(
-        ICommentService commentService,
-        int id)
-    {
-        return Result.Generate(await commentService.DeleteCommentAsync(id));
-    }
-    
-    // -- Result generators for composition --
-    private static async Task<IResult> CreateCommentComposition(
-        ICommentComposition commentComposition,
-        CommentCompositionCreateDto commentCompositionCreateDto)
-    {
-        return Result.Generate(await commentComposition.CreateCommentComposition(commentCompositionCreateDto));
-    }
+		// -- Endpoints for compositions --
+
+		// create a comment with required components and relations.
+		app.MapPost("/api/comment/create/composition", CreateCommentComposition)
+			.AddEndpointFilter<ValidationFilter<CommentCompositionCreateDto>>()
+			.WithName("CreateCommentComposition");
+	}
+
+	private static async Task<IResult> GetCommentsByStablePostId(
+		ICommentService commentService,
+		int id)
+	{
+		return Result.Generate(await commentService.GetCommentByStablePostId(id));
+	}
+
+	private static async Task<IResult> CreateComment(
+		ICommentService commentService,
+		CommentCreateDto commentCreateDto)
+	{
+		return Result.Generate(await commentService.CreateCommentAsync(commentCreateDto));
+	}
+
+	private static async Task<IResult> DeleteComment(
+		ICommentService commentService,
+		int id)
+	{
+		return Result.Generate(await commentService.DeleteCommentAsync(id));
+	}
+
+	// -- Result generators for composition --
+	private static async Task<IResult> CreateCommentComposition(
+		ICommentComposition commentComposition,
+		CommentCompositionCreateDto commentCompositionCreateDto)
+	{
+		return Result.Generate(await commentComposition.CreateCommentComposition(commentCompositionCreateDto));
+	}
 }

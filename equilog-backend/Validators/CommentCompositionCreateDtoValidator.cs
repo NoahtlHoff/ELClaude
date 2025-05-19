c4b924d6d7c04c083f2f7651ed11e930984ae674
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-05-20 00:53:06 +0200

    add commentCompositionCreateDto validator

---
+++ equilog-backend/Validators/CommentCompositionCreateDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using equilog_backend.DTOs.CommentCompositionDTOs;
+using FluentValidation;
+
+namespace equilog_backend.Validators
+{
+	public class CommentCompositionCreateDtoValidator : AbstractValidator<CommentCompositionCreateDto>
+	{
+		public CommentCompositionCreateDtoValidator()
+		{
+			RuleFor(c => c.UserId)
+				.NotEmpty()
+				.GreaterThan(0).WithMessage("User ID must be greater than 0.");
+
+			RuleFor(c => c.StablePostId)
+				.NotEmpty()
+				.GreaterThan(0).WithMessage("User ID must be greater than 0.");
+
+			RuleFor(c => c.Comment)
+				.SetValidator(new CommentCreateDtoValidator());
+		}
+	}
+}

92edf0315475246a155bc4b84a331301bc8c4c0b
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2025-05-20 00:53:28 +0200

    add messages

--- equilog-backend/Validators/HorseCreateDtoValidator.cs
+++ equilog-backend/Validators/HorseCreateDtoValidator.cs
@@ -4,21 +4,23 @@ using FluentValidation;
 namespace equilog_backend.Validators;
 public class HorseCreateDtoValidator : AbstractValidator<HorseCreateDto>
 {
-    public HorseCreateDtoValidator()
-    {
-        RuleFor(h => h.Name)
-            .NotEmpty().WithMessage("Name is required.")
-            .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+	public HorseCreateDtoValidator()
+	{
+		RuleFor(h => h.Name)
+			.NotEmpty().WithMessage("Name is required.")
+			.MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
 
-        RuleFor(h => h.Age)
-            .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today))
-            .When(h => h.Age.HasValue)
-            .WithMessage("Age must be today or a past date.");
+		RuleFor(h => h.Age)
+			.LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today))
+			.When(h => h.Age.HasValue)
+			.WithMessage("Age must be today or a past date.");
 
-        RuleFor(h => h.Color)
-            .MaximumLength(50).When(h => h.Color != null);
+		RuleFor(h => h.Color)
+			.MaximumLength(50).When(h => h.Color != null)
+			.WithMessage("Color cannot exceed 50 characters");
 
-        RuleFor(h => h.Breed)
-            .MaximumLength(50).When(h => h.Breed != null);
-    }
+		RuleFor(h => h.Breed)
+			.MaximumLength(50).When(h => h.Breed != null)
+			.WithMessage("Breed cannot exceed 50 characters");
+	}
 }
